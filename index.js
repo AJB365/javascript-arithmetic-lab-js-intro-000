cf2699d33541272c248955a7459c9375e6ca6f29
Abiola Baruwa <abiola.baruwa.business@gmail.com>
2021-06-30 18:23:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-var add = function (a, b){
-  return a+b;
-}
-var subtract = function (a, b){
-  return a-b;
-}
-var multiply = function (a, b){
-  return  a * b;
+var add = function (a,b) {
+return a + b;
 };
-var divide = function (a, b){
-  return a/b
-}
-var inc = function (a, b){
-  return a+1;
-}
-var dec = function (a, b){
-  return a-1;
-}
-var makeInt = function(n){
-  return parseInt(n, 10);
-}
-var preserveDecimal = function (n){
-  return n;
-}
+
+console.log (add(5,4))
+
+var subtract = function (a,b) {
+  return a - b;
+};
+
+console.log (subtract(5,4))
+
+var multiply = function (a,b) {
+  return a * b;
+};
+
+console.log (multiply(5,4))
+
+var divide = function (a,b) {
+return a / b;
+};
+console.log (divide(5,4))
+
+var inc = function(n) {
+  return n +1;
+};
+
+var dec = function (n) {
+  return n-1;
+};
+
+var makeInt = function(string) {
+  return parseInt (string, 10);
+};
+
+var preserveDecimal = function (decimal) {
+  return parseFloat (decimal, 10);
+};
